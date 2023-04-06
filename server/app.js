5d5d174ebc9e638b8ebf7e3417915914530950ca
wael-benelwafi <wael.benelwafi@adservio.fr>
2023-04-06 13:51:33 +0200

    backend: order api

--- server/app.js
+++ server/app.js
@@ -1,37 +1,40 @@
 require("dotenv").config();
-require("express-async-errors");
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+require('express-async-errors')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 //database
-const connectDB = require("./db/connect");
+const connectDB = require('./db/connect')
 // routes
-const authRouter = require("./routes/authRoutes");
-const usersRouter = require("./routes/usersRoutes");
-const productsRouter = require("./routes/productsRoutes");
+const authRouter = require('./routes/authRoutes')
+const usersRouter = require('./routes/usersRoutes')
+const productsRouter = require('./routes/productsRoutes')
+const OrderRouter = require('./routes/orderRoutes')
 
 //middleware
-const notFoundMiddleware = require("./middleware/not-found");
-const errorHandler = require("./middleware/error-handler");
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandler = require('./middleware/error-handler')
 
-app.set("trust proxy", 1);
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser(process.env.JWT_SECRET));
+app.set('trust proxy', 1)
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
 
-app.get("/", (req, res) => {
-  res.send("ApproAlternative api v1");
-});
-app.get("/api/v1", (req, res) => {
-  res.send("ApproAlternative api");
-});
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/users", usersRouter);
-app.use("/api/v1/products", productsRouter);
+app.get('/', (req, res) => {
+  res.send('ApproAlternative api v1!!')
+})
+app.get('/api/v1', (req, res) => {
+  res.send('ApproAlternative api')
+})
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/products', productsRouter)
+app.use('/api/v1/orders', OrderRouter)
+
 
 
 app.use(notFoundMiddleware);
---
+++ server/models/Order.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
+const SingleOrderItemSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  product: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+})
+
+const OrderSchema = mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subTotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    orderItems: [SingleOrderItemSchema],
+    status: {
+      type: String,
+      enum: ['pending', 'failed', 'paid', 'delivred', 'canceled'],
+      default: 'pending',
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    // clientSecret: {
+    //   type: String,
+    //   required: true,
+    // },
+    // paymentIntentId: {
+    //   type: String,
+    // },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Order', OrderSchema)

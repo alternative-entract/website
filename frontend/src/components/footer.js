2b8cdcd641beb0658bfa1e24435a73a00427a198
Prescilla Lecurieux <prescilla.lecurieux@gmail.com>
2023-05-17 11:10:35 +0200

    handle login & register

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -2,35 +2,35 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer class="py-3 mt-4 bg-light">
-      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item">
-          <Link to="" class="nav-link px-2 text-muted">
+    <footer className="py-3 mt-4 bg-light">
+      <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+        <li className="nav-item">
+          <Link to="" className="nav-link px-2 text-muted">
             Home
           </Link>
         </li>
-        <li class="nav-item">
-          <Link to="" class="nav-link px-2 text-muted">
+        <li className="nav-item">
+          <Link to="" className="nav-link px-2 text-muted">
             Features
           </Link>
         </li>
-        <li class="nav-item">
-          <Link to="" class="nav-link px-2 text-muted">
+        <li className="nav-item">
+          <Link to="" className="nav-link px-2 text-muted">
             Pricing
           </Link>
         </li>
-        <li class="nav-item">
-          <Link to="" class="nav-link px-2 text-muted">
+        <li className="nav-item">
+          <Link to="" className="nav-link px-2 text-muted">
             FAQs
           </Link>
         </li>
-        <li class="nav-item">
-          <Link to="" class="nav-link px-2 text-muted">
+        <li className="nav-item">
+          <Link to="" className="nav-link px-2 text-muted">
             About
           </Link>
         </li>
       </ul>
-      <p class="text-center text-muted">
+      <p className="text-center text-muted">
         © {new Date().getFullYear()} ApproAlternative
       </p>
     </footer>

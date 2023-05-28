7aea7a595192442d97295988ad45beb3cf3ee0b0
Prescilla Lecurieux <prescilla.lecurieux@gmail.com>
2023-05-28 18:17:22 +0200

    rm cloudinary feature

--- frontend/src/utils/cloudinary.tsx
+++
@@ -1,8 +0,0 @@
-import { fill } from "@cloudinary/url-gen/actions/resize";
-import { CloudinaryImage } from "@cloudinary/url-gen";
-
-const getCloudinaryImage = (imageName: string) => {
-        return new CloudinaryImage(imageName, { cloudName: 'dgvuo8wbh' }).resize(fill().width(100).height(150));
-};
-
-export default getCloudinaryImage;

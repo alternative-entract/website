1f16680922268373f86748872e65ab9f8c411b67
Prescilla Lecurieux <34810774+plecrx@users.noreply.github.com>
2023-06-26 13:45:47 +0200

    fix(server): update CORS (#25) (#24)
    * add redirect
    
    * update package.json
    
    * update netlify.toml
    
    * update netlify.toml
    
    * update host CORS
    
    * update host CORS

--- server/src/middleware/hostValidation.js
+++ server/src/middleware/hostValidation.js
@@ -1,7 +1,9 @@
 const hostValidationMiddleware = (req, res, next) => {
-  const allowedHosts = ['your-project-name.netlify.app', 'localhost:8888']
+  const allowedHosts = [
+    'https://alternative-entr-act.netlify.app/',
+    'localhost:8888',
+  ]
   const host = req.headers.host
-  console.log(`host: ${host}`)
 
   if (allowedHosts.includes(host)) {
     next()

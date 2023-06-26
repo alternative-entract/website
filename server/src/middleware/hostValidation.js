123e679611b5a4d1c8318896113a1228bfbc1242
Prescilla Lecurieux <34810774+plecrx@users.noreply.github.com>
2023-06-26 13:25:52 +0200

    chore(ci): add API CI & automatic deploy  (#23)
    * deploy serverless api
    
    * build nevermind..
    
    * add API CI
    
    * fix review
    
    * add redirect
    
    * update ci.yml
    
    * update package.json
    
    * update netlify.toml
    
    * update netlify.toml
    
    * update netlify.toml
    
    * update netlify.toml
    
    * update netlify.toml
    
    * update netlify.toml
    
    * update netlify.toml
    
    * update netlify.toml
    
    * update netlify.toml
    
    * update netlify.toml
    
    * update netlify.toml
    
    * update netlify.toml
    
    * update netlify.toml

--- server/src/middleware/hostValidation.js
+++ server/src/middleware/hostValidation.js
@@ -1,13 +1,12 @@
 const hostValidationMiddleware = (req, res, next) => {
-  const allowedHosts = ['your-project-name.netlify.app', 'localhost:8888'];
-  const host = req.headers.host;
+  const allowedHosts = ['your-project-name.netlify.app', 'localhost:8888']
+  const host = req.headers.host
   console.log(`host: ${host}`)
 
   if (allowedHosts.includes(host)) {
-    next();
-  }
-  else {
-    return res.status(405).send('Host Not Allowed');
+    next()
+  } else {
+    return res.status(405).send('Host Not Allowed')
   }
 }
 

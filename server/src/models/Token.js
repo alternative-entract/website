ef434c5a65fa58a8a3c87c4cec6b580b07bfdaf0
Prescilla Lecurieux <prescilla.lecurieux@gmail.com>
2023-06-27 12:37:36 +0200

    fix prettier

--- server/src/models/Token.js
+++ server/src/models/Token.js
@@ -1,30 +1,30 @@
 const mongoose = require('mongoose')
 
 const TokenSchema = new mongoose.Schema(
-    {
-        refreshToken: {
-            type: String,
-            required: true,
-        },
-        ip: {
-            type: String,
-            required: true,
-        },
-        userAgent: {
-            type: String,
-            required: true,
-        },
-        isValid: {
-            type: Boolean,
-            default: true,
-        },
-        user: {
-            type: mongoose.Schema.ObjectId,
-            ref: 'User',
-            required: true,
-        },
+  {
+    refreshToken: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    ip: {
+      type: String,
+      required: true,
+    },
+    userAgent: {
+      type: String,
+      required: true,
+    },
+    isValid: {
+      type: Boolean,
+      default: true,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
 )
 
 module.exports = mongoose.model('Token', TokenSchema)
